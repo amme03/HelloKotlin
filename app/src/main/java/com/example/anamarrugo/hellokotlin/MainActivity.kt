66e2f95515fc6fce2e4faa1b5f17e0a903abd161
Ana Maria Marrugo Escobar <ana.marrugo@etn.com.co>
2017-11-07 18:06:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/anamarrugo/hellokotlin/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.anamarrugo.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity() : AppCompatActivity(), TextWatcher {
+    override fun afterTextChanged(s: Editable?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    }
+
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
